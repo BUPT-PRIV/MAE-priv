385700c51833efbfbfe588886feca6079aa22fd7
sariel <2788787973@qq.com>
2021-11-21 17:19:50 +0800

    update

--- main_mae.py
+++ main_mae.py
@@ -61,6 +61,8 @@ parser.add_argument('-b', '--batch-size', default=4096, type=int,
                          'using Data Parallel or Distributed Data Parallel')
 parser.add_argument('--lr', '--learning-rate', default=0.00015, type=float,
                     metavar='LR', help='initial (base) learning rate', dest='lr')
+parser.add_argument('--min-lr', '--learning-rate', default=0.0, type=float,
+                    help='minimum learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
 parser.add_argument('--wd', '--weight-decay', default=0.05, type=float,
@@ -437,12 +439,12 @@ class ProgressMeter(object):
         return '[' + fmt + '/' + fmt.format(num_batches) + ']'
 
 
-def adjust_learning_rate(optimizer, epoch, args, final_lr=1e-5):
+def adjust_learning_rate(optimizer, epoch, args):
     """Decays the learning rate with half-cycle cosine after warmup"""
     if epoch < args.warmup_epochs:
         lr = args.lr * epoch / args.warmup_epochs
     else:
-        lr = final_lr + (args.lr - final_lr) * 0.5 * (
+        lr = args.min_lr + (args.lr - args.min_lr) * 0.5 * (
                 1. + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.epochs - args.warmup_epochs)))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
