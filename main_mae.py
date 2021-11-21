92b09f865694d2f872a229e2c6ae5e74edd059c5
sariel <2788787973@qq.com>
2021-11-21 11:31:56 +0800

    bugfix

--- main_mae.py
+++ main_mae.py
@@ -307,7 +307,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
         if not args.multiprocessing_distributed or (args.multiprocessing_distributed
                                                     and args.rank == 0):  # only the first GPU saves checkpoint
-            if epoch % args.save_freq == 0:
+            if epoch % args.save_freq == 0 or epoch == (args.epoch - 1):
                 save_checkpoint({
                     'epoch': epoch + 1,
                     'arch': args.arch,
