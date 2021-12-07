b826870659c8d8afdb41bdd665fe8237bdc7fcb0
flytocc <flytocc@outlook.com>
2021-12-07 15:04:51 +0800

    update save_model

--- tools/run_class_finetuning_prit.py
+++ tools/run_class_finetuning_prit.py
@@ -509,6 +509,10 @@ def main(args, ds_init):
                 utils.save_model(
                     args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                     loss_scaler=loss_scaler, epoch=epoch, model_ema=model_ema)
+            else:
+                utils.save_model(
+                    args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+                    loss_scaler=loss_scaler, epoch=epoch, model_ema=model_ema, epoch_name="latest")
         if data_loader_val is not None:
             test_stats = evaluate(data_loader_val, model, device)
             print(f"Accuracy of the network on the {len(dataset_val)} test images: {test_stats['acc1']:.1f}%")
@@ -517,7 +521,7 @@ def main(args, ds_init):
                 if args.output_dir and args.save_ckpt:
                     utils.save_model(
                         args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                        loss_scaler=loss_scaler, epoch="best", model_ema=model_ema)
+                        loss_scaler=loss_scaler, epoch=epoch, model_ema=model_ema, epoch_name="bast")
             print(f'Max accuracy: {max_accuracy:.2f}%')
 
             log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
